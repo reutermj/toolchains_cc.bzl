385d8e9c953fc3519486f66d8525d57301090f0d
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2025-06-11 08:03:16 -0700

    Check in dockerfiles to build static versions of clang, gcc, and binutils

---
+++ infra/binutils/build_binutils_linux.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -ex
+
+ROOT_DIR=$(pwd)
+
+sudo apk update
+sudo apk add git coreutils diffutils make \
+             gcc g++ bison flex texinfo \
+             zlib-dev
+
+# ===============================
+# || Clone alpine package repo ||
+# ===============================
+APORTS=$ROOT_DIR/aports
+git clone --depth 1 https://gitlab.alpinelinux.org/alpine/aports.git $APORTS
+
+# =================================
+# || Get GCC version from aports ||
+# =================================
+APORTS_BINUTILS_DIR=$APORTS/main/binutils
+BINUTILS_VERSION=$(cat $APORTS_BINUTILS_DIR/APKBUILD | grep "pkgver=" | cut -d'=' -f2 | tr -d '[:space:]' | tr '.' '_')
+
+# ==============================
+# || Download GCC source code ||
+# ==============================
+BINUTILS_DIR=$ROOT_DIR/binutils
+git clone --branch binutils-$BINUTILS_VERSION --depth 1 git://sourceware.org/git/binutils-gdb.git $BINUTILS_DIR
+
+# ==================================================
+# || Apply patches from aports to GCC source code ||
+# ==================================================
+cp -v $APORTS_BINUTILS_DIR/*.patch $BINUTILS_DIR
+
+cd $BINUTILS_DIR
+for patch in *.patch; do
+    # git apply --check $patch
+    git apply $patch || /bin/true
+done
+
+mkdir build
+cd build
+../configure --prefix=/home/bazeler/binutils-out --disable-bootstrap --disable-nls --disable-gdbserver --disable-gdb --disable-gprofng
+make configure-host
+make LDFLAGS="-all-static" -j $(nproc)
+make install
---
+++ infra/gcc/build_gcc_linux.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -ex
+
+ROOT_DIR=$(pwd)
+
+sudo apk update
+sudo apk add git coreutils diffutils make \
+             gcc g++ bison flex texinfo gawk zip gmp-dev mpfr-dev mpc1-dev zlib-dev \
+             linux-headers gmp-dev mpfr-dev mpc1-dev isl-dev zlib-dev libucontext-dev
+
+# ===============================
+# || Clone alpine package repo ||
+# ===============================
+APORTS=$ROOT_DIR/aports
+git clone --depth 1 https://gitlab.alpinelinux.org/alpine/aports.git $APORTS
+
+# =================================
+# || Get GCC version from aports ||
+# =================================
+APORTS_GCC_DIR=$APORTS/main/gcc
+GCC_VERSION=$(cat $APORTS_GCC_DIR/APKBUILD | grep "pkgver=" | cut -d'=' -f2 | tr -d '[:space:]')
+
+# ==============================
+# || Download GCC source code ||
+# ==============================
+GCC_DIR=$ROOT_DIR/gcc
+git clone --depth 1 --branch releases/gcc-$GCC_VERSION git://gcc.gnu.org/git/gcc.git $GCC_DIR
+
+# ==================================================
+# || Apply patches from aports to GCC source code ||
+# ==================================================
+cp -v $APORTS_GCC_DIR/*.patch $GCC_DIR
+
+cd $GCC_DIR
+for patch in *.patch; do
+    # git apply --check $patch
+    git apply $patch || /bin/true
+done
+
+mkdir build
+cd build
+../configure --prefix=$ROOT_DIR/gcc-out --enable-languages=c,c++ --disable-multilib --disable-bootstrap --with-stage1-ldflags="-static" --with-boot-ldflags="-static"
+make -j $(nproc)
+make install
